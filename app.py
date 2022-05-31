10818c24f560dc7c250217b6f97a3adff65ad438
Sarah Stevens <sarah.stevens@wisc.edu>
2022-05-31 16:38:57 -0500

    2nd version of the plot with text and table

--- app.py
+++ app.py
@@ -11,8 +11,14 @@ df = pd.read_csv("Data/gapminder_tidy.csv")
 # filter the data to only Oceania gdpPercap values
 df_filtered = df.query("continent=='Oceania' & metric=='gdpPercap'")
 
+# creating the plot
 title = "GDP for countries in Oceania"
 fig = px.line(df_filtered, x = "year", y = "value", color = "country", title = title, labels={"value": "GDP Percap"})
 st.plotly_chart(fig)
 
+# Line of text describing the plot 
+st.markdown("This plot shows the GDP Per Capita for countries in Oceania.")
 
+
+# Challenge - Display the data table used to create the plot
+st.dataframe(df_filtered)
