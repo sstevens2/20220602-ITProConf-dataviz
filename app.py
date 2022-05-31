e9d9f4c1e4667b4a2164fd4dd5e6c1cfe5db26c3
Sarah Stevens <sarah.stevens@wisc.edu>
2022-05-31 17:12:20 -0500

    adding wdgets for country and metric

--- app.py
+++ app.py
@@ -8,16 +8,23 @@ st.title("Interact with Gapminder Data")
 
 # read in the tidy gapminder data
 df = pd.read_csv("Data/gapminder_tidy.csv")
-# filter the data to only Oceania gdpPercap values
-df_filtered = df.query("continent=='Oceania' & metric=='gdpPercap'")
+
+# list of possible continents
+continent_list = list(df['continent'].unique())
+metric_list = list(df['metric'].unique())
+
+# filter the data to only the data we are plotting
+continent = st.selectbox(label = "Choose a continent", options = continent_list)
+metric = st.selectbox(label = "Choose a metric", options = metric_list)
+df_filtered = df.query(f"continent=='{continent}' & metric=='{metric}'")
 
 # creating the plot
-title = "GDP for countries in Oceania"
-fig = px.line(df_filtered, x = "year", y = "value", color = "country", title = title, labels={"value": "GDP Percap"})
+title = f"{metric} for countries in {continent}"
+fig = px.line(df_filtered, x = "year", y = "value", color = "country", title = title, labels={"value": f"{metric}"})
 st.plotly_chart(fig)
 
-# Line of text describing the plot 
-st.markdown("This plot shows the GDP Per Capita for countries in Oceania.")
+# Line of text describing the plot
+st.markdown(f"This plot shows the {metric} for countries in {continent}.")
 
 
 # Challenge - Display the data table used to create the plot
